5cf0c2263727373f1548b3a2b9feaff4936dd735
Shahwar Spall <shevy.spall@gmail.com>
2019-07-25 04:14:26 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,7 +24,7 @@ def bonus
 
 epic_tragedy[:status] = "dead"
   
-
+return epic_tragedy
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
